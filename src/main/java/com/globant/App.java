662265a35b67c39212e8d6dc6f69898b14298fe0
Andres Felipe Theran Forero <andres.theran@globant.com>
2022-11-16 11:10:24 -0500

    first commit

--- src/main/java/com/globant/App.java
+++
@@ -1,8 +0,0 @@
-package com.globant;
-
-public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ src/test/java/logInTest/LogInTest.java
@@ -0,0 +1,34 @@
+package logInTest;
+
+import org.openqa.selenium.bidi.log.Log;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import Pages.MainPage;
+
+import java.util.logging.Logger;
+
+@Test
+public class LogInTest {
+
+    Logger logger = Logger.getLogger(this.getClass().getName());
+    MainPage mainPage;
+
+    @BeforeMethod
+    @Parameters({"URL"})
+    public void searchPage(String url) {
+        mainPage = new MainPage(url);
+    }
+
+    @Parameters({"username", "password"})
+    @Test
+    public void LogIn(String username, String password) {
+        mainPage.logIn(username, password);
+        Assert.assertEquals(mainPage.isLoggedIn(),Boolean.TRUE);
+    }
+
+    @AfterTest
+    @Parameters({"browser"})
+    public void closeBrowser( String browser ){
+        mainPage.closeBrowser();
+    }
+}
